62c8a01e00d0b93976c9ed22c31e08f143a68a56
marco p parco <pato18djul@gmail.com>
2018-11-10 09:45:34 -0500

    rep

--- 01-javascript/01-Variables.js
+++ 01-javascript/01-Variables.js
@@ -8,7 +8,9 @@ var casado = false;
 var hijos = null;
 var dato= undefined;
 var fechaNacimiento = new Date();
-var marco={"nombre":"marco",
+
+var marco={
+    "nombre":"marco",
     "segundoNombre":"Patricio",
     apellidoPaterno:'Parco',
     apellidoMaterno:'Gualpa',
@@ -19,6 +21,7 @@ var marco={"nombre":"marco",
         nombre:"Vale"}
     };//object
 
+
 console.log("Hola Mundo");
 console.log("edad",typeof edad); //number
 console.log("edadString",typeof edadString); // string
@@ -28,12 +31,15 @@ console.log("hijos",typeof hijos); //object
 console.log("dato valor ",dato);//undefined
 console.log("dato tipo",typeof dato);//undefined
 console.log("fecha Nacimiento",typeof fechaNacimiento);//undefined
+
 console.log(marco.mascota.nombre);
 console.log(marco);
+
 delete marco.hijo;
-console.log(marco);
-marco.hija={nombre:'??'};
-console.log(marco);
+console.log(marco); //eliminar un campo del objetos json
+marco.hija={nombre:'Juana'};
+console.log(marco.hija);
+
 //console.log(marco.abuelo.nombre);
 //console.log(undefined.nombre);
 
@@ -78,6 +84,12 @@ if({nombre:'marco'}){//Truty
     console.log("no");
 }
 if({}){//Truty
+    console.log("Si");
+}else{
+    console.log("no");
+}
+
+if(new Date()){//Truty
     console.log("Si");
 }else{
     console.log("no");
