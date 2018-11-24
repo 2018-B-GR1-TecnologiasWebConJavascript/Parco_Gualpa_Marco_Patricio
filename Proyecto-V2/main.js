e62c0662443d1f55fde3f01f4bd57e9004efcc35
marco p parco <pato18djul@gmail.com>
2018-11-24 06:08:03 -0500

    Comiit

---
+++ Proyecto-V2/main.js
@@ -0,0 +1,34 @@
+//declare var Promise;
+const inquirer = require('inquirer');
+const fs = require('fs');
+const menuPrincipal = {
+    type: 'list',
+    name: 'opcionesMenu',
+    message: 'Seleciona una opcion',
+    choices: [
+        'Ver Banquetes',
+        'Ingresar nuevo Banquete',
+        'Buscar Banquetes',
+        'Actualizar Banquete',
+        'Eliminar Banquete',
+    ]
+};
+const preguntaIngresoBanquete = [
+    {
+        type: 'input',
+        name: 'nombreBanquete',
+        message: "Cual es el nombre del banquete a ingresar ?"
+    },
+    {
+        type: 'input',
+        name: 'costoBanquete',
+        message: "Ingrese el costo del Banquete?"
+    }
+];
+const preguntaBuscarBanquete = [
+    {
+        name: 'nombreRestaurante',
+        type: 'input',
+        message: "Cual es el nombre del Banquete que desea buscar?"
+    }
+];
