681588f1442059b7092f1a3cb74bd8c82d6bc09e
Marco Parco <pato18djul@gmail.com>
2018-12-10 18:00:50 -0500

    Angular

--- 03-Angular/my-dream-app/src/app/app.component.ts
+++ 03-Angular/my-dream-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 //const Component = requir('@angular/core').Component
 import { Component } from '@angular/core';  //TS
-import { } from 'http-server'; //TS
+
 
 @Component({ //decorador es una funcion que se ejecuta antes sirve para ahorrar tiempo
   selector: 'app-root',
